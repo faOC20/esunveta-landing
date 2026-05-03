383bae7d574ae2f369a531eab68c7c2566f30529
faOC20 <fabianortiz.dev@gmail.com>
2026-05-03 01:13:58 -0400

    Revert "feat: vercel insights"
    This reverts commit fd8eb53de505528a8b9822f174ade9aa1b85ea26.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ export default defineConfig({
   integrations: [tailwind()],
   adapter: vercel({
     webAnalytics: { enabled: true },
-    speedInsights: { enabled: true },
   }),
   trailingSlash: "never",
   server: {
